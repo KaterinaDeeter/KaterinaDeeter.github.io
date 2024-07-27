daf73e36005790b35592c78e430e56a1bf33564a
Katerina Deeter <ktdeeter@gmail.com>
2024-07-26 18:01:34 -0700

    Added other website files

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello, world!");
+
+function openlinkedin() {
+  window.open("https://www.linkedin.com/in/katerina-deeter/");
+}
+
+function opengithub() {
+  window.open("https://github.com/KaterinaDeeter");
+}
