a2734af01e5cda986ed35d1697acddd6453169e5
Katerina <KaterinaDeeter@gmail.com>
2026-08-28 15:16:38 -0700

    Removed everything....

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 console.log("Hello, world!");
-
-function openlinkedin() {
-  window.open("https://www.linkedin.com/in/katerina-deeter/");
-}
-
-function opengithub() {
-  window.open("https://github.com/KaterinaDeeter");
-}
